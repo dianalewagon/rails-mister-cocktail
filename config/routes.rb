024906aa41f7074347f19fc8ac0b001eebfa7fcf
Diana King <diana@daytripperstravel.com>
2020-02-20 15:20:24 -0600

    updated tables

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :cocktails, only: [:index, :new, :create, :show, :destroy]
+  # GET "cocktails" (see list)
+  # GET "cocktails/42" (see details)
+  # GET "cocktails/new" (create new)
+  # POST "cocktails" (create new)
+  #resources :ingredient, only: [:index, :show]
+
+  #resources :dose, only: [:index, :new, :create, :destroy]
+  # GET "cocktails/42/doses/new"
+  # POST "cocktails/42/doses"
+  # DELETE "doses/25"
 end
