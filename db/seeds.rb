debb3fc94e41aba8fb2bca8d9dbeda6ca4b469a3
Diana King <diana@daytripperstravel.com>
2020-02-20 16:58:38 -0600

    routes and seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-url'
+require 'json'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+Ingredients.all
